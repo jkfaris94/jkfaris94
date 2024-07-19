5ed4c1c0dfa0dbfd2e1fe5f824a62b596d246c8a
Johnny Faris <155022265+jkfaris94@users.noreply.github.com>
2024-07-19 14:10:17 -0700

    created qualified js file sort-methods.js

---
+++ 3.5.4-sort-methods/sort-methods.js
@@ -0,0 +1,19 @@
+function sortWords(words) {
+
+}
+
+function sortNumbers(numbers) {
+
+}
+
+function largestFirst(numbers) {
+
+}
+
+function sortFlowersByZone(flowers) {
+
+}
+
+function sortByLength(strings) {
+
+}
