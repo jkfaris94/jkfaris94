b7c1455d8dc820f03faac1af6f34bfb2e598f620
Johnny Faris <155022265+jkfaris94@users.noreply.github.com>
2024-07-26 11:55:06 -0700

    playiing around with TDD lesson

--- Test-Files/test.js
+++ Test-Files/test.js
@@ -1,1 +1,42 @@
-console.log(process.argv); 
+const students = [
+    { name: "Shane Carey", score: 9.5 },
+    { name: "Rebecca Mills", score: 8.7 },
+];
+
+// Function being tested
+function highestScore(students) {
+    let result = null;
+    if (!students || !students.length) {
+      return result;
+    }
+  
+    let highScore = 0;
+    for (let i = 0; i < students.length; i++) {
+      let student = students[i];
+      if (student.score > highScore) {
+        highScore = student.score;
+        result = student;
+      }
+    }
+  
+    return result;
+  }
+
+// Setup phase
+const input = [
+  { name: "Shane Carey", score: 9.5 },
+  { name: "Rebecca Mills", score: 8.7 },
+];
+const expected = input[0];
+
+// Run phase
+const actual = highestScore(input);
+
+// Check phase
+if (expected.name === actual.name && expected.score === actual.score) {
+  console.log("Test passed.");
+} else {
+  console.log(
+    "highestScore() did not return the student with the highest score."
+  );
+}
