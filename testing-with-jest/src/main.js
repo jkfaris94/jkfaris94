ef734a67b251459309affba40459bd5ebc4c763c
Johnny Faris <155022265+jkfaris94@users.noreply.github.com>
2024-07-26 12:27:10 -0700

    created test folders

---
+++ testing-with-jest/src/main.js
@@ -0,0 +1,12 @@
+function findHighestScoringStudent(students) {
+    // TODO
+  }
+  
+  module.exports = findHighestScoringStudent;
+
+  const mathStudents = [
+    { name: "Shane Carey", score: 9.5 },
+    { name: "Catarina Lima", score: 9.7 },
+    { name: "Rebecca Mills", score: 8.7 },
+  ];
+  findHighestScoringStudent(mathStudents); //> { name: "Catarina Lima", score: 9.7 }
