63e937cb9c0d70c900d9325b2381e5df772ebe0d
Johnny Faris <155022265+jkfaris94@users.noreply.github.com>
2024-10-17 16:23:38 -0700

    initial upload of 4.3.4 files

---
+++ qualified-4.3.4/JSDOM-_Updating_the_DOM_-Node_18--John_Faris-Solution/src/index.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+/**
+  Write the addheadings() function here
+*/
+
+/**
+  Write the styleTutorialsAndArticles() function here
+*/
+
+/**
+  Write the separateAllTutorials() function here
+*/
+
+/**
+  No need to edit the following
+*/
+function main() {
+  addHeadings();
+  styleTutorialsAndArticles();
+  separateAllTutorials();
+}
+
+main();
