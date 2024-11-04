3df7eec549bc922bebdbc9a57002b1a8f4b93a9c
Johnny Faris <155022265+jkfaris94@users.noreply.github.com>
2024-11-04 12:22:25 -0800

    added the highlightArticle function

--- qualified-4.3.6/index.js
+++ qualified-4.3.6/index.js
@@ -32,6 +32,28 @@ Add event listeners to the `.highlightBtn` buttons
 */
 function highlightArticle() {
   // your code here
+      // Select all buttons with the class '.highlightBtn'
+      const expandButtons = document.querySelectorAll('.highlightBtn');
+
+      // Loop through each button and add a click event listener
+      expandButtons.forEach(button => {
+          button.addEventListener('click', () => {
+              // Find the closest article element to the clicked button
+              const article = button.closest('.article');
+              const articleBody = article.querySelector('.article_body');
+  
+              // Check the current text on the button
+              if (button.textContent === '+') {
+                  // If it's "+",  then add the .highlight class to the article and set the text on the button to -. ">"
+                  article.classList.toggle('highlight');
+                  button.textContent = '-';
+              } else {
+            // Remove the 'highlight' class from the article
+                article.classList.remove('highlight');
+                button.textContent = '+';
+              }
+          });
+      });
 }
 
 function main() {
