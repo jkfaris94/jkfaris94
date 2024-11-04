e8dcb879d399896e6f30a3c01bd0601e8dcfa7ef
Johnny Faris <155022265+jkfaris94@users.noreply.github.com>
2024-11-04 11:56:04 -0800

    initial assessment file upload. excluded import css line  within the index.js

---
+++ qualified-4.3.6/index.js
@@ -0,0 +1,20 @@
+/*
+Add event listeners to the `.expand_button` buttons
+*/
+function expandArticleBody() {
+  // your code here
+}
+
+/*
+Add event listeners to the `.highlightBtn` buttons
+*/
+function highlightArticle() {
+  // your code here
+}
+
+function main() {
+  expandArticleBody();
+  highlightArticle();
+}
+
+main();
